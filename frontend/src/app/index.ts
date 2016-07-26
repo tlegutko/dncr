8c8a8387bd97d8cf26cf3a9b524366a67cb79dff
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2016-07-26 10:53:01 +0200

    [DNCR-47] Building application

--- frontend/src/app/index.ts
+++ frontend/src/app/index.ts
@@ -1,11 +1,6 @@
 // App
 export * from './app.component';
-export * from './app.service';
 export * from './app.routes';
 
-import { AppState } from './app.service';
-
 // Application wide providers
-export const APP_PROVIDERS = [
-  AppState
-];
+export const APP_PROVIDERS = [];
