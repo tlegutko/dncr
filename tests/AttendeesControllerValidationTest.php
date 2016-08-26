62047fc862847339ff9e8452cb966b9b2f979496
tlegutko <tom.legutko@gmail.com>
2016-08-26 09:28:41 +0200

    [DNCR-77] Mocked Validator in tests so that no DB is needed.

--- tests/AttendeesControllerValidationTest.php
+++ tests/AttendeesControllerValidationTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\MessageBag;
+
 class AttendeesControllerValidationTest extends TestCase
 {
   public function testEmptyRequestValidation()
@@ -24,6 +26,7 @@ class AttendeesControllerValidationTest extends TestCase
     $invalidEmailResponse = [
       'Podany adres email jest niepoprawny.',
     ];
+    $this->mockValidator($invalidEmailResponse);
     $this->validationErrorTest($invalidEmailRequest, $invalidEmailResponse);
   }
 
@@ -32,12 +35,14 @@ class AttendeesControllerValidationTest extends TestCase
     $invalidPhoneNumberRequest = [
       'name' => 'Jan',
       'surname' => 'Kowalski',
-      'phoneNumber' => '12312388', // za krotki
+      'phoneNumber' => '12312388',
+      // za krotki
       'email' => 'jan@gmail.com',
     ];
     $invalidPhoneNumberResponse = [
       'Podany numer telefonu jest niepoprawny. Poprawny format to 9 cyfr.',
     ];
+    $this->mockValidator($invalidPhoneNumberResponse);
     $this->validationErrorTest($invalidPhoneNumberRequest, $invalidPhoneNumberResponse);
   }
 
@@ -46,5 +51,16 @@ class AttendeesControllerValidationTest extends TestCase
     $this->json('POST', '/api/attendees', $invalidRequest);
     $this->assertResponseStatus(422);
     $this->seeJsonEquals($expectedResponse);
+  }
+
+  private function mockValidator($expectedResponse) {
+    $messageBag = Mockery::mock('Illuminate\Support\MessageBag');
+    $messageBag->shouldReceive('all')->once()->andReturn($expectedResponse);
+
+    $vaidator = Mockery::mock('Illuminate\Validation\Validator');
+    $vaidator->shouldReceive('passes')->once()->andReturn(false);
+    $vaidator->shouldReceive('errors')->once()->andReturn($messageBag);
+
+    Validator::shouldReceive('make')->once()->andReturn($vaidator);
   }
 }
