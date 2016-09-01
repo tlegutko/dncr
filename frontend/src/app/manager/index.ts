3dffab42535a7f10317806d59aefbb984bf7e4a4
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2016-09-01 09:18:34 +0200

    [DNCR-87] Update application to Angular2 RC5

--- frontend/src/app/manager/index.ts
+++ frontend/src/app/manager/index.ts
@@ -1,4 +1,48 @@
-export * from './manager.component';
-export * from './courses/courses.component';
-export * from './courses/details/details.component';
-export * from './routes';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ManagerComponent } from './manager.component';
+import { ManagerCoursesComponent } from './courses/courses.component';
+import { ManagerCoursesDetailsComponent } from './courses/details/details.component';
+
+// async components must be named routes for WebpackAsyncRoute
+export const routes = [
+  {
+    path: '',
+    component: ManagerComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/manager/courses',
+        pathMatch: 'full'
+      }, {
+        path: 'courses',
+        component: ManagerCoursesComponent,
+        children: [
+          {
+            path: ''
+          }, {
+            path: 'details',
+            component: ManagerCoursesDetailsComponent
+          }
+        ]
+      }
+    ]
+  }
+];
+
+@NgModule(
+  {
+    declarations: [
+      // Components / Directives/ Pipes
+      ManagerComponent, ManagerCoursesComponent, ManagerCoursesDetailsComponent
+    ],
+    imports: [
+      BrowserModule, FormsModule, RouterModule.forChild(routes),
+    ]
+  }
+)
+export default class ManagerModule {
+  static routes = routes;
+}
