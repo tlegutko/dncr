00212b0463710bd7edc52e377d5126c445f9c68f
tlegutko <tom.legutko@gmail.com>
2016-10-09 08:58:57 +0200

    [DNCR-27] Fixed multiple form errors styling.

--- frontend/src/app/_commons/form-field/form-field.component.ts
+++ frontend/src/app/_commons/form-field/form-field.component.ts
@@ -7,7 +7,9 @@ import { Component, Input } from '@angular/core';
       <div class="form-group" [class.has-danger]="errors">
         <ng-content></ng-content>
         <div class="form-control-feedback" *ngIf="errors">
-          <span *ngFor="let error of errors">{{ error }} </span>
+          <ul>
+            <li *ngFor="let error of errors">{{ error }} </li>
+          </ul>
         </div>
       </div>
     `,
