a5f892de7e76cbc1f838dc5abd977ce9799b26e9
tlegutko <tom.legutko@gmail.com>
2016-12-12 18:20:19 +0100

    [DNCR-105] Manual fixes after merge.

--- app/Http/Requests/CreateCourseRequest.php
+++ app/Http/Requests/CreateCourseRequest.php
@@ -18,12 +18,12 @@ class CreateCourseRequest extends Request
       'course.price' => 'required | numeric | min:0',
       'course.classes_count' => 'required | integer | min:1',
       'course.seats_count' => 'required | integer | min:1',
-      'course.instructor_id' => 'required | integer',
       'course.times.*.start_date' => 'required | date',
       'course.times.*.start_time' => 'required | date_format:"H:i"',
       'course.times.*.end_time' => 'required | date_format:"H:i"',
       'course.times.*.repeat_weeks_count' => 'required | integer | min:0',
       'course.times.*.location_id' => 'required | integer | exists:locations,id',
+      'course.times.*.instructors.*.id' => 'required | integer | exists:users,id',
     ];
   }
 
@@ -34,12 +34,12 @@ class CreateCourseRequest extends Request
       'course.price' => 'cena',
       'course.classes_count' => 'liczba zajęć',
       'course.seats_count' => 'liczba miejsc',
-      'course.instructor_id' => 'prowadzący',
       'course.times.*.start_date' => 'data',
       'course.times.*.start_time' => 'rozpoczęcie',
       'course.times.*.end_time' => 'zakończenie',
       'course.times.*.repeat_weeks_count' => 'powtarzaj co',
       'course.times.*.location_id' => 'sala',
+      'course.times.*.instructors.*.id' => 'prowadzący',
     ];
   }
 
@@ -53,8 +53,7 @@ class CreateCourseRequest extends Request
       'seats_count',
     ];
 
-    // TODO remove mock company_id below and inject real one once DNCR-92 is merged
-    return array_only($this->input(['course']), $attributes) + ['company_id' => 1];
+    return array_only($this->input(['course']), $attributes);
   }
 
   function extractCourseTimeData(array $courseTimeData): array
@@ -68,5 +67,14 @@ class CreateCourseRequest extends Request
     ];
 
     return array_only($courseTimeData, $attributes);
+  }
+
+  function extractInstructorIds(array $courseTimeData): array
+  {
+    return array_map(function($instructor)
+    {
+      return $instructor['id'];
+    },
+      $courseTimeData['instructors']);
   }
 }
